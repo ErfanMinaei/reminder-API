da6357892b30f57f76e4be9d9510a71aa97a3481
Erfan <erfanminaei9@gmail.com>
2026-01-28 18:34:04 +0330

    error in const prisma = new PrismaClient({});

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import remindersRouter from "./routers/reminders";
+import { PrismaClient } from "./generated/prisma/client";
 
 const app = express();
+
+const prisma = new PrismaClient({});
 
 app.use(express.json());
 app.use("/reminders", remindersRouter);
